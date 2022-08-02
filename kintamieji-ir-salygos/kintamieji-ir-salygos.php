e8e8e68d6d65554f3f5d32944a425fa1ac5e5152
Aleksandr Vasiljev <zaswet@gmail.com>
2022-08-02 09:30:45 +0300

    homework

--- kintamieji-ir-salygos/kintamieji-ir-salygos.php
+++ kintamieji-ir-salygos/kintamieji-ir-salygos.php
@@ -1,3 +1,17 @@
+<style>
+    .green {
+        color: green;
+    }
+
+    .red {
+        color: red;
+    }
+
+    .blue {
+        color: blue;
+    }
+</style>
+
 <?php
 echo '=====1=====';
 echo '<br>';
@@ -44,21 +58,20 @@ if($a < $b && $b < $c) {
 }else{
     if($c < $b && $b < $a){
         echo "answer : b $b";
-
-    }else{
-        if($c < $a && $a < $b) {
-            echo "answer : a $a";
-            }else{
-                if($b < $a && $a < $c) {
-                    echo "answer : a $a";
+        }else{
+            if($c < $a && $a < $b) {
+                echo "answer : a $a";
                 }else{
-                    if($b < $c && $c < $a){
-                        echo "answer : c $c";
+                    if($b < $a && $a < $c) {
+                        echo "answer : a $a";
                     }else{
-                        if($a < $c && $c < $b){
+                        if($b < $c && $c < $a){
                             echo "answer : c $c";
+                        }else{
+                            if($a < $c && $c < $b){
+                                echo "answer : c $c";
+                            }
                         }
-                    }
 
                 }
             }
@@ -81,3 +94,120 @@ echo $z;
 echo '<br>';
 
 echo $x + $y > $z && $y + $z > $x && $x + $z > $y ? 'Galima' : 'Nigalima';
+
+
+echo '<br>';
+echo '=====5=====';
+echo '<br>';
+
+$A = rand(0, 2);
+echo "A: $A";
+echo '<br>';
+$B = rand(0, 2);
+echo "B: $B";
+echo '<br>';
+$C = rand(0, 2);
+echo "C: $C";
+echo '<br>';
+$D = rand(0, 2);
+echo "D: $D";
+echo '<br>';
+
+$zeroes = 0;
+$ones = 0;
+$twoes = 0;
+if($A === 0){
+    $zeroes++;
+}else{
+    if($A === 1){
+        $ones++;
+    }else{
+        if($A === 2){
+            $twoes++;
+        }
+    }
+}
+if($B === 0){
+    $zeroes++;
+}else{
+    if($B === 1){
+        $ones++;
+    }else{
+        if($B === 2){
+            $twoes++;
+        }
+    }
+}
+if($C === 0){
+    $zeroes++;
+}else{
+    if($C === 1){
+        $ones++;
+    }else{
+        if($C === 2){
+            $twoes++;
+        }
+    }
+}
+if($D === 0){
+    $zeroes++;
+}else{
+    if($D === 1){
+        $ones++;
+    }else{
+        if($D === 2){
+            $twoes++;
+        }
+    }
+}
+
+echo "Zeroes: $zeroes<br>Ones: $ones<br> Twoes: $twoes";
+echo '<br>';
+echo '=====6=====';
+echo '<br>';
+
+$randInHTag = rand(1, 6);
+
+echo "<h3>$randInHTag</h3>";
+
+echo '<br>';
+echo '=====7=====';
+echo '<br>';
+$vienas = rand(-10, 10);
+$du = rand(-10, 10);
+$trys = rand(-10, 10);
+echo"Vienas: $vienas <br>";
+echo"Du: $du <br>";
+echo"Trys: $trys <br>";
+if($vienas < 0){
+    echo '<h1 style="color: green;">'.$vienas.'</h1>';
+}else{
+    if($vienas === 0){
+        echo '<h1 style="color: red;">'.$vienas.'</h1>';
+    }else{
+        echo '<h1 style="color: blue;">'.$vienas.'</h1>';;
+    }
+}
+if($du < 0){
+    echo '<h1 style="color: green;">'.$du.'</h1>';
+}else{
+    if($du === 0){
+        echo '<h1 style="color: red;">'.$du.'</h1>';
+    }else{
+        echo '<h1 style="color: blue;">'.$du.'</h1>';;
+    }
+}
+
+if($trys < 0){
+    echo '<h1 style="color: green;">'.$trys.'</h1>';
+}else{
+    if($trys === 0){
+        echo '<h1 style="color: red;">'.$trys.'</h1>';
+    }else{
+        echo '<h1 style="color: blue;">'.$trys.'</h1>';;
+    }
+}
+
+echo '<br>';
+echo '=====8=====';
+echo '<br>';
