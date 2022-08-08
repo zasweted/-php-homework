0dd6420e56a34ba061d87a9debdabe71448e0e82
Aleksandr Vasiljev <zaswet@gmail.com>
2022-08-08 15:01:16 +0300

    homework

---
+++ funkcijos/functions.php
@@ -0,0 +1,16 @@
+<?php
+echo '<pre>';
+echo '<br>';
+echo '=====1=====';
+echo '<br>';
+
+function greetings($a) {
+    return $a;
+}
+
+$x = greetings('Laba diena');
+
+print_r("<h1>$x</h1>");
+echo '<br>';
+echo '=====2=====';
+echo '<br>';
