4977d84faf6509349620c3b7e59338f5ffbb5036
Aleksandr Vasiljev <zaswet@gmail.com>
2022-08-09 09:03:29 +0300

    homework

--- funkcijos/functions.php
+++ funkcijos/functions.php
@@ -14,3 +14,27 @@ print_r("<h1>$x</h1>");
 echo '<br>';
 echo '=====2=====';
 echo '<br>';
+$y = rand(1, 6);
+function superGreetings($a, $b) {
+    return "<h$b>$a</h$b>";
+}
+
+echo superGreetings($x, $y);
+
+echo '<br>';
+echo '=====3=====';
+echo '<br>';
+
+$rngStr = md5(time());
+
+$answer = preg_replace_callback('/\d+/', function($str){
+    return '<h1>'.$str[0].'</h1>';
+}, $rngStr);
+
+echo $answer;
+
+echo '<br>';
+echo '=====4=====';
+echo '<br>';
+
+
