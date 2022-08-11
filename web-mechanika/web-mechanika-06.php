527bf9225b040990649ae15b665674519ff225d2
Aleksandr Vasiljev <zaswet@gmail.com>
2022-08-11 14:12:14 +0300

    homework

---
+++ web-mechanika/web-mechanika-06.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color:<?= $_SERVER['REQUEST_METHOD'] == 'POST' ? '#FAFA33' : 'lime' ?>">
+    <div>
+        <form action="http://localhost/-php-homework-/-php-homework/web-mechanika/web-mechanika-06.php" method="get"></form>
+            <button type="submit">GET</button>
+    </div>
+    <div>
+        <form action="http://localhost/-php-homework-/-php-homework/web-mechanika/web-mechanika-06.php" method="post"></form>
+            <button type="submit">POST</button>
+    </div>
+</body>
+</html>
---
+++ web-mechanika/web-mechanika-red.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_GET['go'])){
+    header('Location: http://localhost/-php-homework-/-php-homework/web-mechanika/web-mechanika-blue.php');
+    die;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: crimson;">
+<div style="display: flex; flex-direction: column; justify-content: center; align-items: center; margin: 40px 0 0 0;">
+    <a style="color: #000; text-decoration: none;" href="http://localhost/-php-homework-/-php-homework/web-mechanika/web-mechanika-red.php?go">RED</a>
+</div>
+    
+</body>
+</html>
