dc6289238b5b568abce32bbe518b1efeb5bcb2ce
Aleksandr Vasiljev <zaswet@gmail.com>
2022-08-11 12:00:28 +0300

    homework

---
+++ web-mechanika/web-mechanika-lemon.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: yellow; display: flex; flex-direction: column; justify-content: center; align-items: center; margin: 40px 0 0 0;">
+    <a style="color: #000; font-size: 50px;" href="http://localhost/-php-homework-/-php-homework/web-mechanika/web-mechanika-orange.php">Go To Orange</a>
+</body>
+</html>
