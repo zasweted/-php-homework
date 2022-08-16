29e34dadb62438bdce2b223476f8f8b8279cc345
Aleksandr Vasiljev <zaswet@gmail.com>
2022-08-16 12:06:36 +0300

    homework

--- bank-version-01/prideti.php
+++ bank-version-01/prideti.php
@@ -26,28 +26,37 @@ if('POST' == $_SERVER['REQUEST_METHOD']) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/layout.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Document</title>
 </head>
 <body>
-<?php include 'header.php' ?>
-    <h1>Prideti pinigu</h1>
-    <div>
-        <form action="" method="post">
-            <?php foreach(json_decode(file_get_contents(__DIR__ . '/data.json'), 1) as $i => $a) : ?>
-                <?php if($i == $index) :?>
-                    <?php foreach($a as $val) : ?>
-                        <li><?= $val ?></li>
-                        <?php endforeach ?>
-                        <?php endif ?>
-                        <?php endforeach ?>
-                        Prideti pinigu: <input type="number" name="amount" placeholder="Iveskite norima suma" />
-                        <button type="submit">Prideti</button>
+    <section>
+        <?php include 'header.php' ?>
+    </section>
+    <section class="container col-12 row">
+        <div class="container form">
+            <h1>Prideti pinigu</h1>
+            <div>
+                <form action="" method="post">
+                    <?php foreach(json_decode(file_get_contents(__DIR__ . '/data.json'), 1) as $i => $a) : ?>
+                        <?php if($i == $index) :?>
+                            <?php foreach($a as $i => $val) : ?>
+                                <li><span class="list-title"><?= $i?> : </span><span class="list-content"><?=$val?></span></li>
+                                <?php endforeach ?>
+                                <?php endif ?>
+                                <?php endforeach ?>
+                                Prideti pinigu: <input type="number" name="amount" placeholder="Iveskite norima suma" />
+                </form>
+                <button class="btn add" type="submit">Prideti</button>
+            </div>                  
+        </div>
 
-        </form>
-
-    </div>
-    <a href="http://localhost/-php-homework-/-php-homework/bank-version-01/main.php">Grizti i pradzia</a>
-    <a href="http://localhost/-php-homework-/-php-homework/bank-version-01/list.php">Grizti i sarasa</a>
+        <div class="container row col-12">
+            <a class="btn back" href="http://localhost/-php-homework-/-php-homework/bank-version-01/main.php">Grizti i pradzia</a>
+            <a class="btn back" href="http://localhost/-php-homework-/-php-homework/bank-version-01/list.php">Grizti i sarasa</a>
+        </div>
+    </section>
     
 </body>
 </html>
