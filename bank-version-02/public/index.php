56f25b519d6b6f7f7152ab44d2d6ca3d56397496
Aleksandr Vasiljev <zaswet@gmail.com>
2022-08-26 11:30:02 +0300

    homework

--- bank-version-02/public/index.php
+++ bank-version-02/public/index.php
@@ -1,24 +1,32 @@
 <?php
-
 require_once __DIR__ .'/../vendor/autoload.php';
 
 use App\Router;
 
 $router = new \App\Router();
 
-$router->get('home', function () {
+$router->get('', function () {
     require __DIR__ . '/home.php';
+});
+$router->get('home', function () {
+    require __DIR__ . 'home.php';
 });
 $router->get('login', function () {
     require __DIR__ . '/login.php';
 });
-
 $router->get('about', function () {
     echo 'About page lmao';
 });
+
+
+
+
+
+
 
 $router->addNotFoundHandler(function () {
     echo '<h1>' . '404, Page Not Found' . '</h1>';
 });
 
 $router->run();
+
