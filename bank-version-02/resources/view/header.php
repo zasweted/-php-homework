4e2223322e9fd50da417450c837bc77e363ed568
Aleksandr Vasiljev <zaswet@gmail.com>
2022-08-31 12:00:09 +0300

    homework

--- bank-version-02/resources/view/header.php
+++ bank-version-02/resources/view/header.php
@@ -1,10 +1,14 @@
 <section class="container-padding container-row">
     <header class="header">
         <div>
-            <img class="logo" src="<?=URL?>assets/img/logo.png" alt="logo">
-            <a href="<?=URL?>">Home</a>
-            <a href="<?=URL?>create">Create</a>
-            <a href="<?=URL?>list">List</a>
+            <div>
+                <img class="logo" src="<?=URL?>assets/img/logo.png" alt="logo">
+            </div>
+            <div class="nav">
+                <a class="btn" href="<?=URL?>">Home</a>
+                <a class="btn" href="<?=URL?>create">Create</a>
+                <a class="btn" href="<?=URL?>list">List</a>
+            </div>
         </div>
     </header>
 </section>
--- bank-version-02/resources/view/user_list.php
+++ bank-version-02/resources/view/user_list.php
@@ -6,11 +6,13 @@
         <?php foreach($users as $user) : ?>
         <fieldset>
             <div>
-                <li>Vardas: <?=$user['vardas']?></li>
-                <li>Pavarde: <?=$user['pavarde']?></li>
-                <li>Asmens Kodas: <?=$user['asmensKodas']?></li>
-                <li>Saskaitos likutis: <?=$user['pinigai']?> $</li>
-                <li>IBAN: <?=$user['iban']?></li>
+                <li><p class="list-title">Vardas:</p>  <?=$user['vardas']?></li>
+                <li><p class="list-title">Pavarde:</p>  <?=$user['pavarde']?></li>
+                <li><p class="list-title">Asmens Kodas:</p>  <?=$user['asmensKodas']?></li>
+                <li><p class="list-title">Saskaitos likutis:</p>  <?=$user['pinigai']?> $</li>
+                <li><p class="list-title">IBAN:</p>  <?=$user['iban']?></li>
+                <button class="btn btn-green">Koreguoti</button>
+                <button class="btn btn-red">Istrinti</button>
             </div>
         </fieldset>
 
