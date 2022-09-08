f174b3d1fa00aaa65014b1803af2c45f9045b438
Aleksandr Vasiljev <zaswet@gmail.com>
2022-09-08 14:47:57 +0300

    react bank

--- react-bank/src/Components/Nav.jsx
+++ react-bank/src/Components/Nav.jsx
@@ -11,14 +11,13 @@ function Nav() {
             <nav className="navbar" style={{ backgroundColor: '#e3f2fd' }}>
                 <div className="container-fluid nav-custom">
                     <a className="navbar-brand" href="#">
-                        
+
                         <img src={logo2} alt="Logo" style={{
                             width: '90px',
                             height: '72px'
                         }}
                             className="d-inline-block align-text-top App-logo"></img>
                     </a>
-                    <button type="button" className="btn btn-outline-success">Login</button>
                 </div>
             </nav>
         </>
